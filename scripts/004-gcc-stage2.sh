99145f32337de1d9be1a6a4fc50f96faac0d17c4
David Leiter <davidleiter92@gmail.com>
2021-01-30 18:49:29 +0100

    Use LTO and libstdcxx

--- scripts/004-gcc-stage2.sh
+++ scripts/004-gcc-stage2.sh
@@ -47,8 +47,6 @@ rm -rf build-$TARGET-stage2 && mkdir build-$TARGET-stage2 && cd build-$TARGET-st
   --disable-libmudflap \
   --disable-libquadmath \
   --disable-libssp \
-  --disable-libstdcxx-pch \
-  --disable-lto \
   --disable-multilib \
   --disable-nls \
   --disable-shared \
